5cf814a3a65782d2e020b92524519bc270e3ff4b
Dominik Skoda <skoda@d3s.mff.cuni.cz>
2016-01-20 20:25:09 +0100

    Moving the project files one level closer to root dir.

---
+++ src/StudentsDistribution.h
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright 2016 Charles University in Prague
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+/*
+ * StudentsDistribution.h
+ *
+ *  Created on: 17. 1. 2016
+ *      Author: Dominik Skoda <skoda@d3s.mff.cuni.cz>
+ *
+ *  This class provides methods to test hypothesis for given threshold on
+ *  computed Student's distribution.
+ */
+
+#ifndef STUDENTSDISTRIBUTION_H_
+#define STUDENTSDISTRIBUTION_H_
+
+#define MAX_SAMPLES	100
+#define CONFIDENCE 0.95
+
+#include <math.h>
+#include "TTable.h"
+
+class StudentsDistribution {
+public:
+	/*
+	 * df - degrees of freedom
+	 */
+	StudentsDistribution(int df, double mean, double variance);
+	bool isLessThan(double threshold, ALPHAS a);
+	bool isLessThanOrEqual(double threshold, ALPHAS a);
+	bool isGreaterThan(double threshold, ALPHAS a);
+	bool isGreaterThanOrEqual(double threshold, ALPHAS a);
+
+private:
+	int df; // degrees of freedom
+	double mean;
+	double variance;
+	double getICDF(ALPHAS a);
+};
+
+#endif /* STUDENTSDISTRIBUTION_H_ */
+
