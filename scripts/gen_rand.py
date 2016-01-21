d527bc3eac1251c5daf4c35700c161b33b5b5fd5
Tomas Bures <tbures@gmail.com>
2016-01-21 02:22:54 +0100

    Added script for generating random data for testing. The output is to be included as a const in the C code.

---
+++ scripts/gen_rand.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python 
+
+"""
+Generates random x,y samples, where x is drawn from uniform distribution and y is drawn from normal distribution
+
+Usage: get_rand.py <outfile> <number of samples> <beta>
+"""
+
+import numpy as np
+import sys
+
+
+len = int(sys.argv[2])
+beta = float(sys.argv[3])
+
+x = np.sort(np.random.uniform(0, 10000, len))
+y = np.random.normal(2, 3, len) + x*beta
+
+out = open(sys.argv[1], "w")
+out.write("""#define randLen {0}
+int randX[randLen] = {{{1}}};
+double randY[randLen] = {{{2}}};
+""".format(
+	len, 
+	",".join([str(int(v)) for v in x]),
+	",".join([str(v) for v in y])
+))
+
+out.close()
